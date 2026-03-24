888d2df4671a03daf2e9b52ff6f258f1c323e9af
irzokair <irzokair@gmail.com>
2026-03-24 11:25:37 -0300

    ëewfweewfe

---
+++ JavaScript/Slide4.js
@@ -0,0 +1,7 @@
+let nome = "João";
+let Nome = "Maria";
+let NOME = "Carlos";
+
+console.log(nome); // Saída: João
+console.log(Nome); // Saída: Maria
+console.log(NOME); // Saída: Carlos
---
+++ JavaScript/Slide5.js
@@ -0,0 +1,3 @@
+let valor = 10;           // O interpretador identifica como Number
+valor = "Agora sou texto"; // Sem erro, ele muda para String em tempo de execução
+valor = true;              // Agora mudou para Boolean
